eeb4e2eec250c56133dd96fd136817d0a1693d01
preva_l <lisa.prevault@gmail.com>
2018-08-14 16:37:30 +0200

    Coucou

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class DefaultController extends Controller
@@ -9,5 +10,21 @@ class DefaultController extends Controller
     public function homepage()
     {
         return $this->render('default/homepage.html.twig');
+    }
+    
+    //Login function
+
+    public function login(AuthenticationUtils $authUtils){
+
+        $error = $authUtils->getLastAuthenticationError();
+
+            // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+
     }
 }
---
+++ src/Controller/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use App\Form\UserFormType;
+use App\Entity\User;
+use App\Entity\Document;
+
+
+
+class UserController extends Controller{
+
+    public function signin()
+    {
+        return $this->render('signin.html.twig');
+    }
+
+
+    public function userForm( Request $request, EncoderFactoryInterface $factory){
+
+    //Create form with encoded password
+
+        $user = new User;
+        $form = $this->createForm(
+            UserFormType::class,
+            $user,
+            ['standalone'=> true]
+        );
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+
+            $encoder= $factory->getEncoder(User::class);
+            $encodedPassword = $encoder->encodePassword(
+                $user->getPassword(),
+                $user->getUsername()
+            );
+
+            $user->setPassword($encodedPassword);
+
+
+            $file=$user->getPicture();
+            if($file){
+
+                $document = new Document();
+                $document->setPath($this->getParameter('upload_dir'))
+                ->setMimeType($file->getMimeType())
+                ->setName($file->getFilename());
+
+                $file->move($this->getParameter('upload_dir'));
+                $user->setPicture($document);
+                $manager->persist($document);
+            }
+            $manager= $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+            
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('signin.html.twig', 
+        ['user_form'=>$form->createView()]);
+
+
+
+
+
+    }
+
+
+}//class
