1a52b770cf2cd1c66383f5a805a4821dd4bd2819
SyfuxX <dany.thill@live.com>
2018-08-17 15:37:35 +0200

    Fixing some files

--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CharacterRepository")
+ * @ORM\Table(name="user_character")
  */
 class Character
 {
