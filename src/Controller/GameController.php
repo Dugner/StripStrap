44c4c03eef4a4ece9db0edb0b4fb229419f46328
preva_l <lisa.prevault@gmail.com>
2018-08-21 17:49:56 +0200

    game page

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Game;
+use App\Entity\Category;
 
 
 class GameController extends Controller{
@@ -11,10 +12,12 @@ class GameController extends Controller{
     public function gamesList(){
         $manager = $this->getDoctrine()->getManager();
         $gamesList= $manager->getRepository(Game::class)->findAll();
+        $categories= $manager->getRepository(Category::class)->findAll();
 
         return $this->render(
             'Games/games.html.twig',
-            ['gamesList'=> $gamesList]
+            ['gamesList'=> $gamesList,
+            'categories'=> $categories]
         );
     }
 
